41bfebe16ff0c07b190ffb12a9c0622a65427040
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-08 20:25:39 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,5 +25,6 @@ class PostsController < ApplicationController
 	def update
     @post = Post.find(params[:id])
     @post.update(title: params[:title] ,description: params[:description])
+    redirect_to @post
 	end
 end
